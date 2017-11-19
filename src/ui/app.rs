5b690373a393558054af3f10ac3b2e3ed30f6e7c
Lucas Satabin <lucas.satabin@gnieh.org>
2017-11-19 15:26:18 +0100

    Generate password in another thread
    This part looks a bit hacky, because GTK is not thread safe, and the
    password generation code is run in another thread, not to block the UI.

--- src/ui/app.rs
+++ src/ui/app.rs
@@ -17,21 +17,23 @@ use std::path::Path;
 use std::rc::Rc;
 use std::cell::RefCell;
 
+use std::thread;
+use std::sync::mpsc;
+
 use gtk::prelude::*;
 use gtk::*;
 use gio::{Resource, Error, resources_register};
+use glib;
 
 use model::Vault;
 use util::PasswordGenerator;
 
 pub struct App {
-    builder: Builder,
     window: ApplicationWindow,
     new_name: Entry,
     new_password: Entry,
     show_password: CheckButton,
     add_button: Button,
-    password_generator: GeneratorUI,
 
     vault: Option<Rc<Vault>>
 }
@@ -67,18 +69,16 @@ impl App {
         window.set_default_size(800, 600);
         window.set_position(WindowPosition::Center);
 
-        let password_generator = GeneratorUI::new(&new_password);
+        let password_generator = GeneratorUI::new(new_password.clone());
         let expander: Expander = builder.get_object("add-password-options").unwrap();
         expander.add(&password_generator.generator_ui);
 
         let app = App {
-            builder,
             window,
             new_name,
             new_password,
             show_password,
             add_button,
-            password_generator,
 
             vault: None
         };
@@ -99,13 +99,8 @@ impl App {
         {
             let me_too = me.clone();
             me.show_password.connect_toggled(move |check| {
-                let check_too = check.clone();
-                let new_password = me_too.new_password.clone();
-                idle_add(move || {
-                    let show = check_too.get_active();
-                    new_password.set_visibility(show);
-                    Continue(false)
-                });
+                let show = check.get_active();
+                me_too.new_password.set_visibility(show);
             });
         }
 
@@ -119,6 +114,11 @@ impl App {
 
 }
 
+thread_local!(
+    static GEN: RefCell<Option<(GeneratorUI, GenThread)>> = RefCell::new(None)
+);
+
+#[derive(Clone)]
 struct GeneratorUI {
     generator_ui: Box,
     length: SpinButton,
@@ -126,11 +126,13 @@ struct GeneratorUI {
     use_upper: CheckButton,
     use_numbers: CheckButton,
     use_special: CheckButton,
+    spinner: Spinner,
+    password_field: Entry
 }
 
 impl GeneratorUI {
 
-    fn new(new_password: &Entry) -> GeneratorUI {
+    fn new(password_field: Entry) -> GeneratorUI {
         let builder = Builder::new_from_resource("/org/gnieh/Repassync/ui/PasswordGenerator.glade");
 
         let generator_ui: Box = builder.get_object("password-generator-options-box").unwrap();
@@ -145,46 +147,86 @@ impl GeneratorUI {
         let generate_button: Button = builder.get_object("password-generator-generate-button").unwrap();
         let spinner: Spinner = builder.get_object("password-generator-working").unwrap();
 
-        {
-            let length_too = length.clone();
-            let use_lower_too = use_lower.clone();
-            let use_upper_too = use_upper.clone();
-            let use_numbers_too = use_numbers.clone();
-            let use_special_too = use_special.clone();
-            let password_field_bis = new_password.clone();
-            generate_button.connect_clicked(move |_| {
-                ui_bis.set_sensitive(false);
-                spinner.set_visible(true);
-                spinner.start();
-                let mut gen = PasswordGenerator::new(
-                    length_too.get_value_as_int() as usize,
-                    use_lower_too.get_active(),
-                    use_upper_too.get_active(),
-                    use_numbers_too.get_active(),
-                    use_special_too.get_active());
-                let pass = gen.generate();
-                let spinner_bis = spinner.clone();
-                let ui_ter = ui_bis.clone();
-                let password_field_ter = password_field_bis.clone();
-                idle_add(move || {
-                    password_field_ter.set_text(pass.as_str());
-                    spinner_bis.stop();
-                    spinner_bis.set_visible(false);
-                    ui_ter.set_sensitive(true);
-                    Continue(false)
-                });
-            });
-        }
+        let length_too = length.clone();
+        let use_lower_too = use_lower.clone();
+        let use_upper_too = use_upper.clone();
+        let use_numbers_too = use_numbers.clone();
+        let use_special_too = use_special.clone();
+        let spinner_bis = spinner.clone();
 
-        GeneratorUI {
+        let me = GeneratorUI {
             generator_ui,
             length,
             use_lower,
             use_upper,
             use_numbers,
             use_special,
+            spinner,
+            password_field
+        };
+
+        {
+            let me_bis = me.clone();
+            generate_button.connect_clicked(move |_| {
+                ui_bis.set_sensitive(false);
+                spinner_bis.set_visible(true);
+                spinner_bis.start();
+
+                let length = length_too.get_value_as_int() as usize;
+                let use_lower = use_lower_too.get_active();
+                let use_upper = use_upper_too.get_active();
+                let use_numbers = use_numbers_too.get_active();
+                let use_special = use_special_too.get_active();
+
+                let me_ter = me_bis.clone();
+                GEN.with(move |gen| {
+                    *gen.borrow_mut() =
+                        Some((me_ter, GenThread::new(length, use_lower, use_upper, use_numbers, use_special, || { glib::idle_add(generated); })));
+                });
+            });
         }
+
+        me
 
     }
 
+}
+
+fn generated() -> Continue {
+    GEN.with(move |gen| {
+        if let Some((ref ui, ref gen_thread)) = *gen.borrow() {
+            let pass = gen_thread.password_channel.recv().unwrap();
+            ui.password_field.set_text(pass.as_str());
+            ui.spinner.stop();
+            ui.spinner.set_visible(false);
+            ui.generator_ui.set_sensitive(true);
+        }
+        *gen.borrow_mut() = None;
+    });
+    Continue(false)
+}
+
+struct GenThread {
+    password_channel: mpsc::Receiver<String>
+}
+
+impl GenThread {
+
+    fn new<F: Fn() + Send + 'static>(length: usize, use_lower: bool, use_upper: bool, use_numbers: bool, use_special: bool, callback: F) -> Self {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let mut gen = PasswordGenerator::new(
+                length,
+                use_lower,
+                use_upper,
+                use_numbers,
+                use_special);
+            let pass = gen.generate();
+            tx.send(pass).unwrap();
+            callback();
+        });
+
+        GenThread { password_channel: rx }
+    }
 }
